bde87d1e646bd66054fa0f3b7052c37d428bc0b0
Anastasia Roberz <a.roberz@gmx.de>
2026-08-19 14:22:56 +0200

    add eventListener

--- game.js
+++ game.js
@@ -8,3 +8,21 @@ function init() {
 	canvas = document.getElementById("canvas");
 	world = new World(canvas);
 }
+
+window.addEventListener("keydown", (event) => {
+	if (event.code === "ArrowUp") Keyboard.UP = true;
+	if (event.code === "ArrowDown") Keyboard.DOWN = true;
+	if (event.code === "ArrowLeft") Keyboard.LEFT = true;
+	if (event.code === "ArrowRight") Keyboard.RIGHT = true;
+	if (event.code === "Space") Keyboard.SPACE = true;
+});
+
+window.addEventListener("keyup", (event) => {
+	if (event.code === "ArrowUp") Keyboard.UP = false;
+	if (event.code === "ArrowDown") Keyboard.DOWN = false;
+	if (event.code === "ArrowLeft") Keyboard.LEFT = false;
+	if (event.code === "ArrowRight") Keyboard.RIGHT = false;
+	if (event.code === "Space") Keyboard.SPACE = false;
+});
+
+init();
