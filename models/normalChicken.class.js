ccf20a054a6b2a3dc678fc887f898dc2c16d544b
Anastasia Roberz <a.roberz@gmx.de>
2026-08-19 15:29:34 +0200

    add dynamic measurements & positions

--- models/normalChicken.class.js
+++ models/normalChicken.class.js
@@ -2,14 +2,14 @@ import { Chicken } from "./chicken.class.js";
 import { ImageHub } from "./imgHub.class.js";
 
 export class NormalChicken extends Chicken {
-	yPos = 530;
-	width = 102;
-	height = 100;
 	imagesWalk = ImageHub.NORMAL_CHICKEN.walk;
 
-	constructor() {
+	constructor(canvasWidth, canvasHeight) {
 		super();
-		this.loadImg(ImageHub.NORMAL_CHICKEN.walk[0]);
+		this.height = canvasHeight * 0.3;
+		this.width = this.height;
+		this.yPos = canvasHeight * 0.8 - this.height;
+		this.xPos = this.loadImg(ImageHub.NORMAL_CHICKEN.walk[0]);
 		this.loadImages(this.imagesWalk);
 		this.animate();
 	}
