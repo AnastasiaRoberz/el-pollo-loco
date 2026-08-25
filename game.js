dc7507015e6403077e847afa54d8a6e29b50a3e5
Anastasia Roberz <a.roberz@gmx.de>
2026-08-25 08:37:54 +0200

    add start screen

--- game.js
+++ game.js
@@ -1,30 +1,10 @@
-import { Keyboard } from "./models/keyboard.class.js";
 import { World } from "./models/world.class.js";
 
-let canvas;
-let world;
-
 function init() {
-	canvas = document.getElementById("canvas");
-	world = new World(canvas);
+	document.getElementById("start-screen").classList.add("hidden");
+	const canvas = document.getElementById("canvas");
+	canvas.classList.remove("hidden");
+	const world = new World(canvas);
 }
 
-window.addEventListener("keydown", (event) => {
-	if (event.code === "ArrowUp") Keyboard.UP = true;
-	if (event.code === "ArrowDown") Keyboard.DOWN = true;
-	if (event.code === "ArrowLeft") Keyboard.LEFT = true;
-	if (event.code === "ArrowRight") Keyboard.RIGHT = true;
-	if (event.code === "Space") Keyboard.SPACE = true;
-	if (event.code === "KeyS") Keyboard.KEY_S = true;
-});
-
-window.addEventListener("keyup", (event) => {
-	if (event.code === "ArrowUp") Keyboard.UP = false;
-	if (event.code === "ArrowDown") Keyboard.DOWN = false;
-	if (event.code === "ArrowLeft") Keyboard.LEFT = false;
-	if (event.code === "ArrowRight") Keyboard.RIGHT = false;
-	if (event.code === "Space") Keyboard.SPACE = false;
-	if (event.code === "KeyS") Keyboard.KEY_S = false;
-});
-
-init();
+document.getElementById("btn-start-game").addEventListener("click", init);
