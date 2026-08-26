4a3c1351c5f70d2f368d98728d7d10ae54e22970
Anastasia Roberz <a.roberz@gmx.de>
2026-08-26 22:28:15 +0200

    add btn for restarting game

--- game.js
+++ game.js
@@ -1,10 +1,31 @@
 import { World } from "./models/world.class.js";
 
+const gameOverlay = document.getElementById("game-over");
+
 function init() {
 	document.getElementById("start-screen").classList.add("hidden");
+	document.getElementById("game-container").classList.remove("hidden");
 	const canvas = document.getElementById("canvas");
-	canvas.classList.remove("hidden");
+	// const world = new World(canvas, handleGameOver);
 	const world = new World(canvas);
 }
 
-document.getElementById("btn-start-game").addEventListener("click", init);
+init();
+
+function handleGameOver(result) {
+	const imgRef = document.getElementById("result-container");
+	const imgSrc = result === "won" ? "./assets/img/You won, you lost/You Win A.png" : "./assets/img/You won, you lost/You lost.png";
+	gameOverlay.classList.remove("hidden");
+
+	imgRef.innerHTML = /*html*/ `
+		<img class="result-img" src="${imgSrc}" alt="">
+		`;
+}
+
+function restartGame() {
+	gameOverlay.classList.add("hidden");
+	init();
+}
+
+// document.getElementById("btn-start-game").addEventListener("click", init);
+// document.getElementById("btn-restart-game").addEventListener("click", restartGame);
