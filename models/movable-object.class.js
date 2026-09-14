ced59523d1dbed970785d7004c03f106085d28b4
Anastasia Roberz <a.roberz@gmx.de>
2026-09-14 03:27:14 +0200

    fit hurt animation

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -7,7 +7,7 @@ export class MovableObject extends DrawableObject {
 	speedY = 0;
 	acceleration = 2.5;
 	energy = 100;
-	lastHit = 0;
+	lastHit = -1;
 	movementInterval;
 	animationInterval;
 	gravityInterval;
